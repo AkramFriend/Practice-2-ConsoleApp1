c87afa518f5b135a2a4c8b4a1b9f6e03422eeb8d
Акрам <@LAPTOP-36FLJ062>
2024-04-29 01:53:17 +0300

    Добавьте файлы проекта.

---
+++ Practice(2)ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_2_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Акрам Шоев Саидасрорович";
+            sbyte age = 18;
+            string email = "shoevakramchik@gmail.com";
+            float pointsForProgramming = 54.0f;
+            double pointsForMathematics = 64.0;
+            short pointForPhisics = 0;
+            Console.WriteLine($"Ф.И.О: {fullName} \nВозраст: {age} \nEmail: {email} \nБаллы по программирования: {pointsForProgramming} \nБаллы по математике: {pointsForMathematics} \nБаллы по физике: {pointForPhisics}");
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Practice(2.1)ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_2._1_ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sumOfPoints = "s";
+            int s = 64 + 54 + 0;
+            string averagePoint = "n";
+            double n = (double)s / 3;
+            Console.WriteLine($"Сумма баллов по всем предметам: {s} \n Средний балл: {n}");
+            Console.ReadKey();
+        }
+    }
+}
